1857c63861d982e556b6ea5543e8c981f11f8691
Kate Bennert <kate.bennert@gmail.com>
2022-07-27 15:43:44 -0400

    commit tree 7cf68b1874c41e39efab1ad7e9c4707c3fff28d9

--- index.js
+++ index.js
@@ -1,5 +1,52 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employeeInfoArray) {
+    return {
+        firstName: employeeInfoArray[0],
+        familyName: employeeInfoArray[1],
+        title: employeeInfoArray[2],
+        payPerHour: employeeInfoArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    };
+}
+
+function createEmployeeRecords(arrayOfEmployeeInfoArrays) {
+    return arrayOfEmployeeInfoArrays.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+    let hourDate = dateStamp.split(" ");
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hourDate[1]),
+        date: hourDate[0]
+    })
+
+    return this;
+}
+
+function createTimeOutEvent(dateStamp){
+    let hourDate = dateStamp.split(" ");
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hourDate[1]),
+        date: hourDate[0]
+    })
+
+    return this;
+}
+
+function hoursWorkedOnDate(dateWorked) {
+    let employeeOut = this.timeOutEvents.find(dateElement => dateElement.date === dateWorked);
+    let employeeIn = this.timeInEvents.find(dateElement => dateElement.date === dateWorked);
+    return (employeeOut.hour - employeeIn.hour) / 100;
+}
+
+function wagesEarnedOnDate(dateWorked) {
+    return hoursWorkedOnDate.call(this, dateWorked) * this.payPerHour;
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
@@ -20,4 +67,19 @@ const allWagesFor = function () {
 
     return payable
 }
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(employeeRecords) {
+    let totalWageArray = employeeRecords.map(employee => allWagesFor.call(employee));
+    const initialValue = 0;
+    return totalWageArray.reduce((previousValue, currentValue) => previousValue + currentValue, initialValue);
+}
+
+
+
+
+
 
